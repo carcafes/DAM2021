79336a8f87374584e48a7d55be0fea42dcffa1df
Cosme <carcafes@gmail.com>
2021-11-21 14:31:05 +0000

    aa

---
+++ AUT01_02 Comunicación entre procesos en Java/SocketLector/src/socketlector/Main.java
@@ -0,0 +1,82 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package socketlector;
+
+import java.net.*;
+import java.io.*;
+/**
+ *
+ * @author usuario
+ */
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    	Process procesoComando2;
+        Runtime builder = Runtime.getRuntime();
+        String cmd = "tr 'a'  'z'";
+        String linea;
+    	
+    	// TODO code application logic here
+        Socket canal = null; //Socket para establecer el canal de conexión con el escritor
+        BufferedReader entrada = null; //Para el stream de lectura
+        String valorEntrada = null; //Valores que iremos leyendo del canal        
+        try{
+            canal = new Socket("localhost", 12345 );
+            //Pedimos establecer una conexión en el equipo local con el puerto 12345
+            //donde debe estar escuchando el proceso escritor
+            }catch (Exception ex){
+                System.err.println("No se ha podido establecer conexión.");
+                System.err.println(ex.toString());
+        }
+        if (canal != null)  //Si hemos podido establecer la conexión. Tenemos
+                            //un canal de comunicación
+            try{
+               entrada = new BufferedReader(new InputStreamReader(canal.getInputStream()));
+               //Obtemenos el objeto que representa el strean de entrada en el canal
+               //Lector con buffer, para no perder ningún dato
+               
+               procesoComando2 = builder.exec(cmd);
+             //Escribir en la escucha del proceso
+               OutputStream os = procesoComando2.getOutputStream();
+               OutputStreamWriter osw = new OutputStreamWriter(os);
+               PrintWriter pw = new PrintWriter(osw);
+               while ((linea = entrada.readLine()) != null){
+            	   pw.write(linea); //Mandamos del 0 al 9
+               } 
+               pw.close();
+               
+               
+               BufferedReader bf = new BufferedReader(new InputStreamReader(procesoComando2.getInputStream()));
+               
+               
+               while ((linea = bf.readLine()) != null){
+                   System.out.println(linea); //Mandamos del 0 al 9
+               }                
+            }catch (Exception ex){
+               System.err.println("No se ha podido establecer conexión.");
+               System.err.println(ex.toString());
+            }finally{
+                //Nos aseguramos de que se cierran los recursos que estamos utilizando
+                if (entrada != null)
+                    try{
+                        entrada.close();
+                    }catch (IOException ex) {
+                        System.err.println("Se ha producido un error al cerrar el InputStreamReader.");
+                        System.err.println(ex.toString());
+                    }
+                if (canal != null)
+                    try{
+                        canal.close();
+                    }catch (IOException ex) {
+                        System.err.println("Se ha producido un error al cerrar el Socket.");
+                        System.err.println(ex.toString());
+                    }
+            }
+    }
+
+}
